cff248df12c2a790f7684c372f4ac84483a933e3
Henrik Tudborg <henrik@tudb.org>
2015-04-25 00:38:18 +0200

    Only show popular by full rows

--- resources/hot.py
+++ resources/hot.py
@@ -4,6 +4,7 @@ from falcon.util import uri
 from jinja2 import Environment, FileSystemLoader
 from util import clean_text
 
+ROW_WIDTH = 4
 
 class Resource(object):
 
@@ -13,9 +14,12 @@ class Resource(object):
         self.env = Environment(loader=self.loader)
 
     def on_get(self, req, resp):
-            print (self.stats.get_top(4*4))
+            popular = self.stats.get_top(ROW_WIDTH * 10)
+
+            rows = len(popular) // ROW_WIDTH
+
             context = {
-                "popular": self.stats.get_top(4*4)
+                "popular":  popular[:(rows*ROW_WIDTH)]
             }
             resp.set_header("Cache-Control", "max-age=3600")
             resp.set_header("Content-Type", "text/html; charset=utf8")
