0b98ef2dbf543738d4cf88f4f9c7357a07bfd2ad
Henrik Tudborg <henrik@tudb.org>
2015-04-24 18:54:52 +0200

    Render can now do thumbnails

--- resources/render.py
+++ resources/render.py
@@ -8,7 +8,8 @@ from wand.color import Color
 from wand.font import Font
 from wand.drawing import Drawing
 
-CACHE_SIZE = 64  # MB
+CACHE_SIZE = 48  # MB
+THUMBNAIL_CACHE_SIZE = 24  # MB
 TEXT_MAX_LEN = 100
 
 HEX_COLOR_LIME = "#9AC61E"
@@ -19,25 +20,137 @@ COLOR_LIME = Color(HEX_COLOR_LIME)
 COLOR_DARK = Color(HEX_COLOR_DARK)
 COLOR_DIM = Color(HEX_COLOR_DIM)
 
+
 WIDTH_HEIGHT_RATIO = 1.5
-OUTPUT_WIDTH = 600
-OUTPUT_HEIGHT = int(OUTPUT_WIDTH * WIDTH_HEIGHT_RATIO)
-FONT_WIDTH_RATIO = 0.23
-FONT_SIZE = int(OUTPUT_WIDTH * FONT_WIDTH_RATIO)
+IMAGE_WIDTH = 600
+IMAGE_HEIGHT = int(IMAGE_WIDTH * WIDTH_HEIGHT_RATIO)
 
-PATH_FONT = "./fonts/Raleway-ExtraBoldItalic.ttf"
-LZY_PATH_FONT = "./fonts/Raleway-Medium.ttf"
-PATH_IMG_STOP = "./images/stop.svg"
-PATH_IMG_PAYOFF = "./images/payoff.png"
-PATH_IMG_NAME = "./images/name.png"
-PATH_IMG_LOGO = "./images/logo.png"
-PATH_IMG_DICKBUTT = "./images/dickbutt.png"
+THUMBNAIL_WIDTH = 100
+THUMBNAIL_HEIGHT = int(THUMBNAIL_WIDTH * WIDTH_HEIGHT_RATIO)
 
-IMG_STOP_WIDTH_TARGET = int(0.66*OUTPUT_WIDTH)
-IMG_PAYOFF_WIDTH_TARGET = int(0.20*OUTPUT_WIDTH)
-IMG_NAME_WIDTH_TARGET = int(0.28*OUTPUT_WIDTH)
-IMG_LOGO_WIDTH_TARGET = int(0.12*OUTPUT_WIDTH)
-IMG_DICKBUTT_WIDTH_TARGET = int(0.05*OUTPUT_WIDTH)
+
+class ImageLayout(object):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.ratio = height/width
+
+        self.font_size = int(self.width * 0.23)
+
+        self.img_stop_width_target = int(0.66*width)
+        self.img_payoff_width_target = int(0.20*width)
+        self.img_name_width_target = int(0.28*width)
+        self.img_logo_width_target = int(0.12*width)
+        self.img_dickbutt_width_target = int(0.05*width)
+
+        self.path_font = "./fonts/Raleway-ExtraBoldItalic.ttf"
+        self.lzy_path_font = "./fonts/Raleway-Medium.ttf"
+
+        self.path_img_stop = "./images/stop.svg"
+        self.path_img_payoff = "./images/payoff.png"
+        self.path_img_name = "./images/name.png"
+        self.path_img_logo = "./images/logo.png"
+        self.path_img_dickbutt = "./images/dickbutt.png"
+
+        self._base_image = None
+
+    @property
+    def base_image(self):
+        if not self._base_image:
+            self._base_image = self.compose_base_image()
+        return self._base_image
+
+    def compose_base_image(self):
+        # compose the base image
+
+        # "stop" image
+        img_stop = Image(filename=self.path_img_stop)
+        scale = int(img_stop.height * self.img_stop_width_target / img_stop.width)
+        img_stop.resize(self.img_stop_width_target, scale)
+
+        # "payoff" image
+        img_payoff = Image(filename=self.path_img_payoff)
+        scale = int(img_payoff.height * self.img_payoff_width_target / img_payoff.width)
+        img_payoff.resize(self.img_payoff_width_target, scale)
+
+        # name
+        img_name = Image(filename=self.path_img_name)
+        scale = int(img_name.height * self.img_name_width_target / img_name.width)
+        img_name.resize(self.img_name_width_target, scale)
+
+        # logo
+        img_logo = Image(filename=self.path_img_logo)
+        scale = int(img_logo.height * self.img_logo_width_target / img_logo.width)
+        img_logo.resize(self.img_logo_width_target, scale)
+
+        # dickbutt
+        img_dickbutt = Image(filename=self.path_img_dickbutt)
+        scale = int(img_dickbutt.height * self.img_dickbutt_width_target / img_dickbutt.width)
+        img_dickbutt.resize(self.img_dickbutt_width_target, scale)
+
+        # calc the offsets
+        self.stop_y_offset = int(self.height*0.11)
+        self.stop_x_offset = 0
+
+        self.text_y_offset = int(self.stop_y_offset+img_stop.height)
+        self.text_x_offset = 0
+
+        self.name_y_offset = int(self.height*0.88)
+        self.name_x_offset = int(self.width*0.08)
+
+        self.payoff_y_offset = int(self.height*0.888)
+        self.payoff_x_offset = int(self.width*0.57)
+
+        self.logo_y_offset = int(self.height*0.858)
+        self.logo_x_offset = int(self.width*0.80)
+
+        self.dickbutt_x_offset = int(self.width*0.24)
+        self.dickbutt_y_offset = int(self.height*0.9)
+
+        # compose the base image
+        base_img = Image(
+            height=self.height,
+            width=self.width,
+            background=COLOR_DARK)
+
+        base_img.composite(img_stop,
+                           left=self.stop_x_offset,
+                           top=self.stop_y_offset)
+
+        base_img.composite(img_name,
+                           left=self.name_x_offset,
+                           top=self.name_y_offset)
+        base_img.composite(img_payoff,
+                           left=self.payoff_x_offset,
+                           top=self.payoff_y_offset)
+        base_img.composite(img_logo,
+                           left=self.logo_x_offset,
+                           top=self.logo_y_offset)
+
+        if self.width >= 300:
+            base_img.composite_channel(
+                channel="rgb_channels",
+                image=img_dickbutt,
+                left=self.dickbutt_x_offset,
+                top=self.dickbutt_y_offset,
+                operator='blend')
+
+        if self.width >= 200:
+            with Drawing() as footer_draw:
+                footer_draw.font = self.lzy_path_font
+                footer_draw.font_size = 14
+                footer_draw.fill_color = COLOR_DIM
+                footer_draw.gravity = "south_east"
+                footer_draw.text(5, 5, "csigerstop.dk")
+                footer_draw.draw(base_img)
+
+        img_stop.destroy()
+        img_payoff.destroy()
+        img_name.destroy()
+        img_logo.destroy()
+        img_dickbutt.destroy()
+
+        return base_img
 
 
 class DataCache(object):
@@ -73,101 +186,18 @@ class DataCache(object):
 class Resource(object):
 
     def __init__(self):
-        self.cache = DataCache(1024*1024*CACHE_SIZE)
-        self._base_img = None
-
-        # calc the offsets
-        self.stop_offset = 0
-        self.text_offset = 0
-        self.name_offset = 0
-        self.payoff_offset = 0
-        self.logo_offset = 0
+        self.image_layout = ImageLayout(IMAGE_WIDTH, IMAGE_HEIGHT)
+        self.thumbnail_layout = ImageLayout(THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT)
+        self.image_cache = DataCache(1024*1024*CACHE_SIZE)
+        self.thumbnail_cache = DataCache(1024*1024*THUMBNAIL_CACHE_SIZE)
 
     @property
     def base_img(self):
-        if not self._base_img:
-            # compose the base image
+        return self.image_layout.base_image
 
-            # "stop" image
-            img_stop = Image(filename=PATH_IMG_STOP)
-            scale = int(img_stop.height * IMG_STOP_WIDTH_TARGET / img_stop.width)
-            img_stop.resize(IMG_STOP_WIDTH_TARGET, scale)
-
-            # "payoff" image
-            img_payoff = Image(filename=PATH_IMG_PAYOFF)
-            scale = int(img_payoff.height * IMG_PAYOFF_WIDTH_TARGET / img_payoff.width)
-            img_payoff.resize(IMG_PAYOFF_WIDTH_TARGET, scale)
-
-            # name
-            img_name = Image(filename=PATH_IMG_NAME)
-            scale = int(img_name.height * IMG_NAME_WIDTH_TARGET / img_name.width)
-            img_name.resize(IMG_NAME_WIDTH_TARGET, scale)
-
-            # logo
-            img_logo = Image(filename=PATH_IMG_LOGO)
-            scale = int(img_logo.height * IMG_LOGO_WIDTH_TARGET / img_logo.width)
-            img_logo.resize(IMG_LOGO_WIDTH_TARGET, scale)
-
-            # dickbutt
-            img_dickbutt = Image(filename=PATH_IMG_DICKBUTT)
-            scale = int(img_dickbutt.height * IMG_DICKBUTT_WIDTH_TARGET / img_dickbutt.width)
-            img_dickbutt.resize(IMG_DICKBUTT_WIDTH_TARGET, scale)
-
-            # calc the offsets
-            self.stop_offset = int(OUTPUT_HEIGHT*0.11)
-            self.text_offset = int(self.stop_offset+img_stop.height)
-            self.name_offset = int(OUTPUT_HEIGHT*0.88)
-            self.payoff_offset = int(OUTPUT_HEIGHT*0.888)
-            self.logo_offset = int(OUTPUT_HEIGHT*0.858)
-
-            dickbutt_x_offset = int(OUTPUT_WIDTH*0.24)
-            dickbutt_y_offset = int(OUTPUT_HEIGHT*0.9)
-
-            # compose the base image
-            base_img = Image(
-                height=OUTPUT_HEIGHT,
-                width=OUTPUT_WIDTH,
-                background=COLOR_DARK)
-
-            base_img.composite(img_stop, left=0, top=self.stop_offset)
-
-            base_img.composite(img_name,
-                               left=int(OUTPUT_WIDTH*0.08),
-                               top=self.name_offset)
-            base_img.composite(img_payoff,
-                               left=int(OUTPUT_WIDTH*0.57),
-                               top=self.payoff_offset)
-            base_img.composite(img_logo,
-                               left=int(OUTPUT_WIDTH*0.80),
-                               top=self.logo_offset)
-
-            base_img.composite_channel(
-                channel="rgb_channels",
-                image=img_dickbutt,
-                left=dickbutt_x_offset,
-                top=dickbutt_y_offset,
-                operator='blend')
-
-            with Drawing() as footer_draw:
-                footer_draw.font = LZY_PATH_FONT
-                footer_draw.font_size = 14
-                footer_draw.fill_color = COLOR_DIM
-                footer_draw.gravity = "south_east"
-                footer_draw.text(5, 5, "csigerstop.dk")
-                footer_draw.draw(base_img)
-
-            img_stop.destroy()
-            img_payoff.destroy()
-            img_name.destroy()
-            img_logo.destroy()
-
-            self._base_img = base_img
-
-        return self._base_img
-
-    def draw_text(self, img, text):
+    def draw_text(self, layout, img, text):
         with Drawing() as draw:
-            draw.font = PATH_FONT
+            draw.font = layout.path_font
             draw.fill_color = COLOR_LIME
             draw.text_antialias = True
             draw.gravity = "north_west"
@@ -182,10 +212,10 @@ class Resource(object):
                 if len(line) < 1:
                     continue
 
-                draw.font_size = FONT_SIZE
+                draw.font_size = layout.font_size
                 fm = draw.get_font_metrics(img, line)
                 iter_protect = 20
-                while fm.text_width > OUTPUT_WIDTH and iter_protect > 0:
+                while fm.text_width > layout.width and iter_protect > 0:
                     draw.font_size -= draw.font_size / 10
                     fm = draw.get_font_metrics(img, line)
                     iter_protect -= 1
@@ -198,7 +228,7 @@ class Resource(object):
                 else:
                     spare_room_top = int(fm.text_height * 0.15)
 
-                line_offset = self.text_offset + offset - spare_room_top
+                line_offset = layout.text_y_offset + offset - spare_room_top
 
                 draw.text(0, line_offset, line)
 
@@ -209,8 +239,19 @@ class Resource(object):
             draw.draw(img)
 
     def on_get(self, req, resp):
+
+        is_thumbnail = bool(req.get_param("thumbnail"))
+
+        if is_thumbnail:
+            cache = self.thumbnail_cache
+            layout = self.thumbnail_layout
+        else:
+            cache = self.image_cache
+            layout = self.image_layout
+
         image_data = None
 
+        # cache hack
         if req.get_header("If-None-Match") and req.get_header("Cache-Control") != "no-cache":
             resp.status = falcon.HTTP_304
             return
@@ -226,12 +267,13 @@ class Resource(object):
             text = re.sub(r" +", "\n", text)
         text = re.sub(r"_+", " ", text)
 
-        image_data = self.cache.get(text)
+
+        image_data = cache.get(text)
         if not image_data:
-            with self.base_img.clone() as canvas:
-                self.draw_text(canvas, text)
+            with layout.base_image.clone() as canvas:
+                self.draw_text(layout, canvas, text)
                 image_data = canvas.make_blob("png")
-            self.cache.set(text, image_data)
+            cache.set(text, image_data)
 
         resp.set_header("Cache-Control", "public, max-age=3600")
         resp.set_header("Content-Type", "image/png")
@@ -240,8 +282,8 @@ class Resource(object):
 
     def on_post(self, req, resp):
         stats = [
-            "%d items in cache" % len(self.cache.cache),
-            "%f MB in cache" % ((self.cache.current_capacity or 0.1) / 1024 / 1024),
+            "%d items in cache" % len(self.image_cache.image_cache),
+            "%f MB in cache" % ((self.image_cache.current_capacity or 0.1) / 1024 / 1024),
         ]
 
         resp.body = "\n".join(stats)
